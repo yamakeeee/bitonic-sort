42927daf231180f25ffbd9e18c208e4ead09509e
yamake <yamake@gmail.com>
2022-12-07 09:50:36 +0900

    feat:bitonic sort の GPU 実装まで

--- bitonic_sort/main.cu
+++ bitonic_sort/main.cu
@@ -1,57 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <algorithm>
 
 #include "cuda_runtime.h"
 
-static void bitonic_sort_GPU(int *in, int*out, const int n);
-// static void bitonic_sort_CPU(int *in, int*out, const int n);
+static void bitonic_sort_GPU(int *in, int*out, const int n, const int m);
+static void bitonic_sort_CPU(int *in, int*out, const int n, const int m);
 
-int main()
+bool solve()
 {
-    printf("Hello from CPU\n");
+    const int m = 10;
+    const int n = (1 << m);
 
-    const int n = 1024;
-    int *in = new int[n];
+    int *a = new int[n];
     int *out_cpu = new int[n];
     int *out_gpu = new int[n];
 
-    for (int i = 0; i < n; i++) in[i] = rand() % 10000;
+    for (int i = 0; i < n; i++) a[i] = rand() % 10000;
 
-    bitonic_sort_GPU(in, out_gpu, n);
-    // bitonic_sort_CPU(in, out_cpu, n);
+    bitonic_sort_GPU(a, out_gpu, n, m);
+    bitonic_sort_CPU(a, out_cpu, n, m);
 
-    delete[] in;
+    std::sort(a, a + n);
+
+    for(int i = 0; i < n; ++i){
+        if(a[i] != out_cpu[i] || a[i] != out_gpu[i]){
+            std::cout<<"NG"<<std::endl;
+            printf("a[%d], out_cpu[%d], out_gpu[%d] = %d, %d, %d\n", i, i, i, a[i], out_cpu[i], out_gpu[i]);
+            return false;
+        }
+    }
+
+    delete[] a;
     delete[] out_cpu;
     delete[] out_gpu;
-
-    return 0;
+    return true;
 }
 
-__global__ void kernel_bitonic_sort(int *in, int *out, const int n)
+__global__ void kernel_bitonic_sort(int *a, const int n, const int c, const int j)
 {
     int i = blockIdx.x * blockDim.x +threadIdx.x;
-    if (i < n) {
-        out[i] = in[i] * 2;
+    if (i < n / 2) {
+        int idx = i + ((i >> j) << j);
+        if (((i >> c) & 1) == 0 && a[idx] > a[idx + (1 << j)]){
+            int tmp = a[idx];
+            a[idx] = a[idx + (1 << j)];
+            a[idx + (1 << j)] = tmp;
+        }
+        else if(((i >> c) & 1) && a[idx] < a[idx + (1 << j)]){
+            int tmp = a[idx];
+            a[idx] = a[idx + (1 << j)];
+            a[idx + (1 << j)] = tmp;
+        }
     }
 }
 
-static void bitonic_sort_GPU(int *hIn, int *hOut, const int n)
+static void bitonic_sort_GPU(int *hIn, int *hOut, const int n, const int m)
 {
-    int *dIn;
-    int *dOut;
-    cudaMallocHost((void**)&dIn, n * sizeof(int));
-    cudaMallocHost((void**)&dOut, n * sizeof(int));
-    cudaMemcpy(dIn, hIn, n * sizeof(int), cudaMemcpyHostToDevice);
-    int m=(n+31)/32;
-    kernel_vecDouble<<<32, m>>>(dIn, dOut, n);
+    int *dArray;
+    cudaMallocHost((void**)&dArray, n * sizeof(int));
+    cudaMemcpy(dArray, hIn, n * sizeof(int), cudaMemcpyHostToDevice);
+    int blockSize = (n + 31) / 32;
+    for(int c = 0; c < m; ++c){
+        for(int j = c; j >= 0; --j){
+            kernel_bitonic_sort<<<32, blockSize>>>(dArray, n, c, j);
+        }
+    }
     cudaDeviceSynchronize();
-
-    cudaMemcpy(hOut, dOut, n * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaFree(dIn);
-    cudaFree(dOut);
+    cudaMemcpy(hOut, dArray, n * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaFree(dArray);
 }
 
-// static void bitonic_sort_CPU(int *hIn, int *hOut, const int n){
-//     return;
-// }
+static void bitonic_sort_CPU(int *hIn, int *a, const int n, const int m){
+    for(int i = 0; i < n; ++i)a[i] = hIn[i];
+    for(int c = 0; c < m; ++c){
+        for(int j = c; j >= 0; --j){
+            for(int i = 0; i < (n >> 1); ++i){
+                int idx = i + ((i >> j) << j);
+                // printf("idx, idx + (1 << j) = %d, %d\n", idx, idx + (1 << j));
+                if (((i >> c) & 1) == 0 && a[idx] > a[idx + (1 << j)]){
+                    std::swap(a[idx], a[idx + (1 << j)]);
+                }
+                else if(((i >> c) & 1) && a[idx] < a[idx + (1 << j)]){
+                    std::swap(a[idx], a[idx + (1 << j)]);
+                }
+            }
+        }
+    }
+    return;
+}
+
+int main(){
+    int n = 1000;
+    int ng = 0;
+    for(int i = 0; i < n; ++i){
+        if(!solve())++ng;
+    }
+    std::cout << ng << std::endl;
+}
